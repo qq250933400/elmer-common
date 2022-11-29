f8e2ae1dd91ca061621668f932af2fb34c0f2ee1
elmer s j mo <250933400@qq.com>
2022-11-29 17:42:24 +0800

    update decoration logic

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,16 +1,2 @@
-import { Autowired as DCAutowired } from "./Autowired";
-import { Model as DCModel } from "./Model";
-import { Plugin as DCPlugin } from "./Plugin";
-import { Service as DCService } from "./Service";
-
-export const Autowired = DCAutowired;
-export const Model = DCModel;
-export const Service = DCService;
-export const Plugin = DCPlugin;
-
-export default {
-    Autowired: DCAutowired,
-    Model: DCModel,
-    Plugin: DCPlugin,
-    Service: DCService
-};
+export { Autowired } from "./Autowired";
+export * from "./base";
